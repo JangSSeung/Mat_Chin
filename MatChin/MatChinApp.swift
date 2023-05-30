27925e27b7bc73c02ad140df20c79fd0f9f79a8f
tmdgh <tmdgh1xn@gmail.com>
2023-05-30 22:01:49 +0900

    Initial Commit

---
+++ MatChin/MatChinApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatChinApp.swift
+//  MatChin
+//
+//  Created by 승호 on 2023/05/30.
+//
+
+import SwiftUI
+
+@main
+struct MatChinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
